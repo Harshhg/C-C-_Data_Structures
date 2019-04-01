f775c64f63127e76abf31c5732564d14f3b85d7b
Harsh Gupta <myfolder.pc@gmail.com>
2019-04-01 14:25:07 +0530

    optimized time complexity of code

--- rotating_array.c
+++ rotating_array.c
@@ -1,36 +1,28 @@
 #include <stdio.h>
-
 int main() {
-	//code
-
 	int tc; //number of test cases
 	scanf("%d",&tc); //inputing test cases
 	while(tc--)
 	{
 	    int n; //number of arrays elements
 	    scanf("%d",&n); //inputting array elements
+	    int d; //number of times to rotate the array;
+	    scanf("%d",&d);
 	    int ar[n],temp;
 	    for(int i=0;i<n; i++)
 	        scanf("%d",&ar[i]);
-	    int d; //number of times to rotate the array;
-	    scanf("%d",&d);
-	    while(d--)
-	    {
-	        int i=0;
-	        temp= ar[i];
-	        for(i=0;i<n-1;i++)
-	        {
-	           ar[i]=ar[i+1];
-	        }
-	        ar[i]=temp;
-	    }
-
-	
+	    int b[n];
+	    
+	    for(int i=0;i<n;i++)
+	       {
+	           if(d==n)
+	            d = 0;
+	            b[i]=ar[d++];
+	         }
+	        
 	for(int i=0; i<n;i++)
-	    printf("%d ",ar[i]);
-	 printf("\n");
+	    printf("%d ",b[i]);
+    printf("\n");
 	}
-	
-	
 	return 0;
 }
